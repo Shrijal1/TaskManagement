c12b78b677d592031e6149e512da7f62dbfd4815
Shrijal <shrijalbhattarai10@gmail.com>
2024-04-22 22:34:13 +0545

    initial commit

---
+++ src/container/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="loginWrapper">
+      <div className="loginContainer">
+        <div className="loginFormWrapper">Login</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
